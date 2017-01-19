804d9ab84cca870448a800794314eaf3fd929cc6
Adriana Matsufuji <adrianabm@gmail.com>
2017-01-19 17:02:09 +0100

    Form validation

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 import * as actions from '../../actions'
 
+const validate = formProps => {
+  const errors = {}
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Passwords must match'
+  }
+
+  return errors
+}
+
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <div>
+    <input {...input} type={type} className="form-control"/>
+    {touched && error && <span className="error">{error}</span>}
+  </div>
+)
+
 class Signup extends Component {
+
   render() {
     const { handleSubmit } = this.props
 
@@ -11,22 +28,19 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Email:</label>
           <Field name="email"
-            component="input"
-             className="form-control" />
+            component={renderField} />
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
           <Field name="password"
-            component="input"
-            type="password"
-            className="form-control" />
+            component={renderField}
+            type="password" />
         </fieldset>
         <fieldset className="form-group">
           <label>Password Confirmation:</label>
           <Field name="passwordConfirm"
-            component="input"
-            type="password"
-            className="form-control" />
+            component={renderField}
+            type="password" />
         </fieldset>
         <button action="submit" className="btn btn-primary">Sign Up!</button>
       </form>
