f3dfa93951f900496f420f5b6e7a2d7a9c0e1252
Adriana Matsufuji <adrianabm@gmail.com>
2017-01-19 20:15:45 +0100

    Feature component requires auth

---
+++ src/components/feature.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Feature extends Component {
+  render() {
+    return (
+      <div>
+        I'm feature
+      </div>
+    )
+  }
+}
+
+export default Feature
--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import reduxThunk from 'redux-thunk'
 import App from './components/app'
 import SignIn from './components/auth/signin'
 import SignUp from './components/auth/signup'
+import RequireAuth from './components/auth/require_auth'
+import Feature from './components/feature'
 import reducers from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
@@ -18,6 +20,7 @@ ReactDOM.render(
       <Route path="/" component={ App }>
         <Route path="/signin" component={ SignIn } />
         <Route path="/signup" component={ SignUp } />
+        <Route path="/feature" component={ RequireAuth(Feature) } />
       </Route>
     </Router>
   </Provider>
