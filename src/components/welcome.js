ff310247c0a5d3214adf735f96c6aa8a036b1f82
Adriana Matsufuji <adrianabm@gmail.com>
2017-01-20 13:28:12 +0100

    Index route and welcome component

---
+++ src/components/welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Feature extends Component {
+  render() {
+    return (
+      <div>
+        Welcome to our slice of paradise!
+      </div>
+    )
+  }
+}
+
+export default Feature
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './components/app'
 import SignIn from './components/auth/signin'
 import SignUp from './components/auth/signup'
 import RequireAuth from './components/auth/require_auth'
+import Welcome from './components/welcome'
 import Feature from './components/feature'
 import reducers from './reducers'
 
@@ -18,6 +19,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={ browserHistory }>
       <Route path="/" component={ App }>
+        <IndexRoute component={ Welcome } />
         <Route path="/signin" component={ SignIn } />
         <Route path="/signup" component={ SignUp } />
         <Route path="/feature" component={ RequireAuth(Feature) } />
